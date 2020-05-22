31fae0f6ba3dc2eadb87e61835cd44793675935f
Sravya2007 <65549677+Sravya2007@users.noreply.github.com>
2020-05-22 20:42:47 +0530

    Change in formula

--- sketch.js
+++ sketch.js
@@ -26,7 +26,7 @@ function draw() {
   if(hasCollided(bullet, wall)){
     bullet.velocityX = 0;
 
-    var damage = 0.5 * weight * speed * speed/(thickness * thickness * thickness);
+    var damage = (0.5 * weight * speed * speed)/(thickness * thickness * thickness);
 
     if(damage < 10) {
       bullet.shapeColor = "green";
